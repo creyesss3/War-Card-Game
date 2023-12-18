9eab627a3098293f3a7222ee3f545b23b30ccbd7
Christian Reyes <christian7reyes@berkeley.edu>
2023-12-18 14:46:04 -0800

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Christian Reyes on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Christian Reyes on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
