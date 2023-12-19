9c7a8e391889a3cccf676d58ce5827df7d9ea0dc
Christian Reyes <christian7reyes@berkeley.edu>
2023-12-19 10:24:59 -0800

    v1.1

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     @State var playerScore: Int = 0
     @State var cpuScore: Int = 0
     
+    @State var gameOver: Bool = false
+    
     var body: some View {
         
         ZStack {
@@ -41,32 +43,49 @@ struct ContentView: View {
                 }
                 
                 Spacer()
-                HStack {
-                    Spacer()
-                    VStack {
-                        Text("Player")
-                            .font(.headline)
-                            .padding(.bottom, 10.0)
-                        Text(String(playerScore))
-                            .font(.largeTitle)
-                            
+                
+                if gameOver {
+                    Text(winnerMessage())
+                        .font(.title)
+                        .foregroundColor(.white)
+                } else {
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Text("Player")
+                                .font(.headline)
+                                .padding(.bottom, 10.0)
+                            Text(String(playerScore))
+                                .font(.largeTitle)
+                                
+                        }
+                        Spacer()
+                        VStack {
+                            Text("CPU")
+                                .font(.headline)
+                                .padding(.bottom, 10.0)
+                            Text(String(cpuScore))
+                                .font(.largeTitle)
+                        }
+                        Spacer()
                     }
-                    Spacer()
-                    VStack {
-                        Text("CPU")
-                            .font(.headline)
-                            .padding(.bottom, 10.0)
-                        Text(String(cpuScore))
-                            .font(.largeTitle)
-                    }
-                    Spacer()
+                    .foregroundColor(.white)
                 }
-                .foregroundColor(.white)
+                
                 Spacer()
             }
         }
     }
+    
     func deal() {
+        guard !gameOver else {
+            // Reset the game
+            playerScore = 0
+            cpuScore = 0
+            gameOver = false
+            return
+        }
+        
         // Randomize the player's card
         let playerCardValue = Int.random(in: 2...14)
         playerCard = "card" + String(playerCardValue)
@@ -86,8 +105,32 @@ struct ContentView: View {
         }
         print("Player Score: \(playerScore)")
         print("CPU Score: \(cpuScore)")
+
+        // Check for game over
+        if playerScore >= 10 || cpuScore >= 10 {
+            gameOver = true
+        }
+    }
+    
+    func winnerMessage() -> String {
+        if playerScore > cpuScore {
+            return "Player wins!"
+        } else if cpuScore > playerScore {
+            return "CPU wins!"
+        } else {
+            return "It's a tie!"
+        }
     }
 }
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
 
 #Preview {
     ContentView()
