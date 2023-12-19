1d7365a3a9ff45a9830314617eae3ae6ec999ca6
Christian Reyes <christian7reyes@berkeley.edu>
2023-12-19 10:21:22 -0800

    v1.0

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard: String = "card" + String(Int.random(in: 2...14))
+    @State var cpuCard: String = "card" + String(Int.random(in: 2...14))
+    
+    @State var playerScore: Int = 0
+    @State var cpuScore: Int = 0
+    
     var body: some View {
+        
         ZStack {
             Image("background-plain")
                 .resizable()
@@ -19,13 +27,19 @@ struct ContentView: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image("button")
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
                 Spacer()
                 HStack {
                     Spacer()
@@ -33,7 +47,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             
                     }
@@ -42,7 +56,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -51,6 +65,27 @@ struct ContentView: View {
                 Spacer()
             }
         }
+    }
+    func deal() {
+        // Randomize the player's card
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomize the CPU's card
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+
+        print("Player Card: \(playerCardValue)")
+        print("CPU Card: \(cpuCardValue)")
+
+        // Update the score
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        } else if cpuCardValue > playerCardValue {
+            cpuScore += 1
+        }
+        print("Player Score: \(playerScore)")
+        print("CPU Score: \(cpuScore)")
     }
 }
 
